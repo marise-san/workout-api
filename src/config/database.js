7b513e219889c0120fb071348294b8091a9fd7eb
marise-san <marisemrtns@gmail.com>
2025-03-03 14:02:02 -0300

    Muda a url para a publica.

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DB_RAILWAY_URL,
-  ssl: process.env.DB_RAILWAY_URL?.includes("railway") ? { rejectUnauthorized: false } : false,
-  port: process.env.DB_RAILWAY_PORT
+  connectionString: process.env.DB_PUBLIC_URL,
+  ssl: process.env.DB_PUBLIC_URL?.includes("railway") ? { rejectUnauthorized: false } : false,
+  port: process.env.DB_PUBLIC_PORT
 });
 
 pool.connect()
